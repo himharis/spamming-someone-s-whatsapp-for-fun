d2b1ed46db2de6ddaf38672d9f25645bd9acc9ae
Abdul Haris Ibrahim <63350306+himharis@users.noreply.github.com>
2022-04-07 23:13:18 -0400

    Update wa_bot.py

--- wa_bot.py
+++ wa_bot.py
@@ -3,8 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(20) 
 driver.get('https://web.whatsapp.com')
-driver.find_element_by_css_selector("span[title='Ruslan Fkm 12']").click()
-inputString = "piu"
+driver.find_element_by_css_selector("span[title='your target number in your phone']").click()
+inputString = "the message you want to send to them"
 while(True):
     driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(inputString)
     driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
